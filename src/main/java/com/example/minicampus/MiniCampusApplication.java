b1ae05feed2499ed3b241a22fcea81d542630f58
SoA-Lee <llsa0927@naver.com>
2022-09-28 20:20:32 +0900

    init

---
+++ src/main/java/com/example/minicampus/MiniCampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.minicampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minicampus/MiniCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minicampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
